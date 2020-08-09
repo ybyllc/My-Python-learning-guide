dc0f6f3a6f5e965fdfab46c19eeb43fa250d30c0
一杯原谅绿茶 <ybyllc@users.noreply.github.com>
2020-08-09 21:08:33 +0800

    我的修改

--- 草稿本.py
+++
@@ -1,6 +0,0 @@
-name = input ("ÄãËûÂè½ĞÊ²Ã´ÍæÒâ¶ù£¿")
-print("Hello"+ name)
-#shutil.copytree('','')#????
-    git config --global user.name "ybyllc"
-    git config --global user.email "ybyllc@github.com"
-    ssh-keygen -t rsa -C "ybyllc@github.com" //
